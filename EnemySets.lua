d0ae1b77d3e8332dde7b30478f139559a76829b3
Hex-anon <119101146+Hex-anon@users.noreply.github.com>
2022-11-26 17:56:02 +1000

    Add files via upload

---
+++ EnemySets.lua
@@ -0,0 +1,316 @@
+EnemySets =
+{
+	AllEliteAttributes =
+	{ 
+		"Blink",
+		"Blink",
+		"HeavyArmor",
+		"HeavyArmor",
+		"ExtraDamage",
+		"ExtraDamage",
+		"DeathSpreadHitShields",
+		"DeathSpreadHitShields",
+		"Frenzy",
+		"Frenzy",
+		"Vacuuming",
+		"Vacuuming",
+		"Beams",
+		"Beams",
+
+		-- Tartarus Only
+		"Disguise",
+		"Disguise",
+		"Disguise",
+
+		-- Asphodel Only
+		"Molten",
+		"Molten",
+
+		-- Elysium Only
+		--"MultiEgg",
+	},
+	RangedOnlyEliteAttributes =
+	{
+		"Homing",
+		"Homing",
+		"Homing",
+		"Homing",
+		"Homing",
+	},
+	ShadeOnlyEliteAttributes =
+	{
+		"MultiEgg",
+		"MultiEgg",
+		"MultiEgg",
+		"MultiEgg",
+	},
+
+	EliteAttributesRunBanOptions =
+	{
+		"Blink",
+		"HeavyArmor",
+		"ExtraDamage",
+		"DeathSpreadHitShields",
+		"Frenzy",
+		"Vacuuming",
+		"Beams",
+
+		"Homing",
+
+		"Disguise",
+
+		"Molten",
+
+		"MultiEgg",
+	},
+
+	EnemiesBiome1 =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	EnemiesBiome1Devotion =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome1Survival =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	EnemiesBiome1Hard =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	EnemiesBiome1Thanatos =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome1MiniBoss =
+	{
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome2 =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	EnemiesBiome2Survival =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	EnemiesBiome2Challenge =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome2Thanatos =
+	{
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+		"FlurrySpawnerSuperElite",
+	},
+
+	EnemiesBloodlessElite =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	EnemiesBiome2Wrapping =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	EnemiesBiome2Devotion =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome2Hard =
+	{
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome3 =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome3Survival =
+	{
+		"FlurrySpawnerSuperElite",
+	},
+
+	EnemiesBiome3Hard =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome3Devotion =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome4 =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawner",
+		"FlurrySpawnerElite",
+	},
+
+	EnemiesBiome4MiniBoss =
+	{
+        "FlurrySpawnerSuperElite",
+	},
+
+	EnemiesBiome4MiniBossFodder =
+	{
+        "FlurrySpawner",
+	},
+
+	EnemiesBiome4Mini =
+	{
+	"FlurrySpawner",
+        "FlurrySpawnerElite",
+	},
+
+	EnemiesBiome4MiniHard =
+	{
+        "FlurrySpawnerElite",
+	},
+
+	EnemiesBiome4MiniSingle =
+	{
+        "FlurrySpawnerElite",
+	},
+
+	EnemiesBiome4Traversal =
+	{
+        "FlurrySpawner",
+	},
+
+	--[[ Organized by Bink Memory
+	EnemiesHadesLarge =
+	{   
+        "FlurrySpawnerElite",
+  	},
+
+	EnemiesHadesSmall =
+	{
+        "FlurrySpawner",
+	},
+	]]
+
+	-- Organized By Difficulty
+	EnemiesHadesLarge =
+	{   
+        "FlurrySpawnerElite",
+  	},
+
+	EnemiesHadesSmall =
+	{
+        "FlurrySpawner",
+	},
+
+	EnemiesHadesEMLarge =
+	{   
+        "FlurrySpawnerSuperElite",
+  	},
+
+	EnemiesHadesEMSmall =
+	{
+        "FlurrySpawnerSuperElite",
+	},
+
+	ShrineChallengeSuperElite =
+	{ 
+        --"FlurrySpawnerSuperElite",
+	},
+
+	ShrineChallengeFodder =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	ShrineChallengeTartarus =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	ShrineChallengeAsphodel =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	ShrineChallengeElysium =
+	{
+		"FlurrySpawner",
+		"FlurrySpawner",
+	},
+
+	HydraHeads =
+	{
+		"HydraHeadDartmaker", "HydraHeadLavamaker", "HydraHeadSummoner", "HydraHeadSlammer", "HydraHeadWavemaker"
+	},
+
+	TrapsBiome1 =
+	{
+		"SpikeTrap",
+	},
+
+	TrapsBiome2 =
+	{
+		"GasTrapPassive",
+	},
+
+	Minibosses =
+	{
+		"RatThugMiniboss",
+		"ThiefImpulseMineLayerMiniboss",
+		"SatyrRangedMiniboss",
+		"HeavyRangedForkedMiniboss",
+		"CrawlerMiniBoss",
+
+		"WretchAssassinMiniboss",
+		"HeavyRangedSplitterMiniboss",
+
+		"HitAndRunUnitElite",
+		"CrusherUnitElite",
+		"SpreadShotUnitMiniboss",
+		
+		"FlurrySpawnerElite",
+	},
+}
